6d904db738d730c8933754d26d47ff94e2a876ff
Aaryn <chrybym@gmail.com>
2025-09-26 23:16:43 -0700

    Update Lexer.h
    Added the token struct

--- HydrgenLex/Lexer.h
+++ HydrgenLex/Lexer.h
@@ -1,6 +1,9 @@
 #ifndef HYDLEX_H
 #define HYDLEX_H
-enum tokentype {
+
+// Why am I coding this on a chromebook 11:16 pm.
+
+typedef enum class {
     // Keywords
     IF, ELSE, ELSIF, WHILE, FOR, DO, CLASS, ENUM,
     UINT8, UINT16, UINT32, UINT64, UINT128, INT8,
@@ -17,5 +20,12 @@ enum tokentype {
     ROLLEFT, ROLRIGHT, AND, OR, XOR, NOT, LAND, LOR,
     LNOT, NAND, NOR, NXOR, LNAND, LNOR
 };
+
+typedef struct {
+    tokentype type; // the type of the token
+    char* lexeme; // the actual code
+    int vpos; // the vertical position slash the column
+    int hpos; // the horizontal position slash the line
+} token;
 
 #endif
